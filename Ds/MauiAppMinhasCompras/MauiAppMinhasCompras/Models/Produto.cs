71323cda082db5eb35f0a03c54bd96eb594018db
Lucas Silva <lucaobrsantos@gmail.com>
2025-03-30 22:37:19 -0300

    Construção da classe para fazer CRUDE no SQLite

---
+++ Ds/MauiAppMinhasCompras/MauiAppMinhasCompras/Models/Produto.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace MauiAppMinhasCompras.Models
+{
+    public class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public Double Quantidade { get; set; }
+
+        public Double Preco { get; set; }
+    }
+}
